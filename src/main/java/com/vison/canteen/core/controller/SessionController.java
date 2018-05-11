0929ee4bb38ca2cd75c5c332453b6c68b5fba05d
SniperVison <hauvison@163.com>
2018-05-11 21:46:11 +0800

    修改强制下线功能

--- src/main/java/com/vison/canteen/core/controller/SessionController.java
+++ src/main/java/com/vison/canteen/core/controller/SessionController.java
@@ -5,9 +5,11 @@ import com.vison.canteen.biz.bean.UserOnline;
 import com.vison.canteen.core.bean.PO.UserPO;
 import com.vison.canteen.core.service.SessionService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -40,7 +42,6 @@ public class SessionController {
     }
 
 
-    @RequiresPermissions("user:kickout")
     @PostMapping("forceLogout")
     public ResponseDTO forceLogout(String id) {
         try {
